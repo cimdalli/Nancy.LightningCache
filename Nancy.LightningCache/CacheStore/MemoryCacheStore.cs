6b59eb71f01a46b59dd04414fdbd96192c5e54e0
okan.cetin <okan.cetin@hepsiburada.com>
2015-12-29 18:23:28 +0200

    #000 okan adds MemoryCacheStore and remove asp dependencies on package config

---
+++ Nancy.LightningCache/CacheStore/MemoryCacheStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy.LightningCache.Projection;
+
+namespace Nancy.LightningCache.CacheStore
+{
+    public class MemoryCacheStore : ICacheStore
+    {
+        private static readonly Dictionary<string, SerializableResponse> Cache = new Dictionary<string, SerializableResponse>();
+        private readonly object _lock = new object();
+
+        public CachedResponse Get(string key)
+        {
+            lock (_lock)
+            {
+                if (Cache.ContainsKey(key))
+                {
+                    return new CachedResponse(Cache[key]);
+                }
+            }
+            return null;
+        }
+
+        public void Set(string key, NancyContext context, DateTime absoluteExpiration)
+        {
+            lock (_lock)
+            {
+                if (Cache.ContainsKey(key))
+                {
+                    Remove(key);
+                }
+
+                Cache.Add(key, new SerializableResponse(context.Response, absoluteExpiration));
+
+                Cache.Where(pair => pair.Value.Expiration < DateTime.Now).Select(pair => pair.Key).ToList().ForEach(Remove);
+            }
+        }
+
+        public void Remove(string key)
+        {
+            lock (_lock)
+            {
+                if (Cache.ContainsKey(key))
+                {
+                    Cache.Remove(key);
+                }
+            }
+        }
+    }
+}
